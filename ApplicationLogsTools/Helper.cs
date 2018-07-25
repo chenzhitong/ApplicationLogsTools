6231ea38d7960bf4555f768f68a76ab4c90b91a7
陈志同 <chenzhitong12@hotmail.com>
2018-07-25 11:37:34 +0800

    initial commit

---
+++ ApplicationLogsTools/Helper.cs
@@ -0,0 +1,39 @@
+﻿using Neo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLogsTools
+{
+    public static class Helper
+    {
+        public static string HexToString(this string hex)
+        {
+            if (hex.Length % 2 != 0)
+            {
+                throw new ArgumentException();
+            }
+            var output = "";
+            for (int i = 0; i <= hex.Length - 2; i += 2)
+            {
+                try
+                {
+                    var result = Convert.ToByte(new string(hex.Skip(i).Take(2).ToArray()), 16);
+                    output += (Convert.ToChar(result));
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return output;
+        }
+        public static string ToHexString(this string str)
+        {
+            byte[] byteArray = Encoding.Default.GetBytes(str.ToCharArray());
+            return byteArray.ToHexString();
+        }
+    }
+}
---
+++ ApplicationLogsTools/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Win32;
+using Neo;
+using Neo.Wallets;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ApplicationLogsTools
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private string FileName;
+
+        private void TextInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var input = new StringBuilder(TextInput.Text);
+            var json = JObject.Parse(TextInput.Text);
+            for (int i = 0; i < json["notifications"].Count(); i++)
+            {
+                var stateValue = json["notifications"][i]["state"]["value"];
+                stateValue[0]["value"] = (stateValue[0]["value"].ToString()).HexToString();
+                stateValue[1]["value"] = Wallet.ToAddress(new UInt160(stateValue[1]["value"].ToString().HexToBytes()));
+                stateValue[2]["value"] = Wallet.ToAddress(new UInt160(stateValue[2]["value"].ToString().HexToBytes()));
+                stateValue[3]["value"] = new BigInteger("00e1f505".HexToBytes()).ToString();
+            }
+            TextOutput.Text = json.ToString();
+        }
+
+        private void Grid_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effects = DragDropEffects.Link; 
+            else e.Effects = DragDropEffects.None;
+        }
+
+        private void Grid_Drop(object sender, DragEventArgs e)
+        {
+            FileName = ((Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            var text = File.ReadAllText(FileName);
+            TextInput.Text = text;
+            SaveMenu.IsEnabled = true;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                FileName = openFileDialog.FileName;
+                var text = File.ReadAllText(FileName);
+                TextInput.Text = text;
+                SaveMenu.IsEnabled = true;
+            }
+        }
+
+        private void SaveMenu_Click(object sender, RoutedEventArgs e)
+        {
+            File.WriteAllText(FileName, TextOutput.Text);
+            StatusText.Text = "Save Successed";
+        }
+    }
+}
